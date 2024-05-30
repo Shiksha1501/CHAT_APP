57c71e0b7dd7b43ba5a03ffac244ef4702a6599a
Shiksha1501 <shikshavishwakarma1501@gmail.com>
2024-05-31 03:22:51 +0530

    added search user and createchat APIs

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -4,11 +4,12 @@ const {
   authUser,
   allUsers,
 } = require("../controllers/userControllers");
-// const authMiddleware = require("../middleware/authMiddleware");
+const { protect } = require("../middlewares/authMiddleware.js");
+//const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-// router.route("/").get(protect, allUsers);
+router.route("/").get(protect, allUsers);
 router.route("/").post(registerUser);
 router.post("/login", authUser);
 
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 dotenv.config();
 connectDB();
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
