4b8711736920f96440d311ef3c53a86ca3f95a2b
Tarushi <tarushijha97@gmail.com>
2024-05-30 21:36:54 +0530

    user registration and auth

---
+++ Backend/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+// Ensure environment variables are loaded (if using a .env file)
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.bold);
+    process.exit(1); // Exit with a non-zero status code to indicate an error
+  }
+};
+
+module.exports = connectDB;
