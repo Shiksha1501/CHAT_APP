14cba256f0b51d1427a00f35b5aa9e0bbec8733d
Tarushi <tarushijha97@gmail.com>
2024-06-02 21:09:41 +0530

    added socket.IO and modifications done in frontend and backend

--- Backend/server.js
+++ Backend/server.js
@@ -39,7 +39,7 @@ const io = require("socket.io")(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log("connected to socket.io");
+  console.log("socket.io connected");
 
   socket.on("setup", (userData) => {
     socket.join(userData._id);
